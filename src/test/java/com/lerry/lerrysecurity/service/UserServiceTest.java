d85af2e6c179a911d7b54a829f0489dcd7fdb4ac
lerry903 <lerryli@foxmail.com>
2018-07-07 11:51:27 +0800

    使用spring-boot-api-project-seed作为基础架构(https://github.com/lerry903/spring-boot-api-project-seed)

---
+++ src/test/java/com/lerry/lerrysecurity/service/UserServiceTest.java
@@ -0,0 +1,18 @@
+package com.lerry.lerrysecurity.service;
+
+import com.lerry.lerrysecurity.BaseTestService;
+
+public class UserServiceTest extends BaseTestService {
+
+    /*
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void findOne() throws Exception {
+        User user = userService.findById(1);
+        Assert.assertEquals(Integer.valueOf(1), user.getId());
+    }
+    */
+
+}
